ff1a0e84cd4d85bb79b9b55c55c8b6cebf7953ca
Lucas Araujo <lucasara1707@gmail.com>
2024-10-19 18:51:04 -0300

    adicionando componentes onclick no react

--- src/components/ItemSuggestion.tsx
+++ src/components/ItemSuggestion.tsx
@@ -1,10 +1,21 @@
 
-type ItemSuggestionProps = {
-    title: string
-}
+import { ComponentProps } from "react" 
 
-export function ItemSuggestion({title}: ItemSuggestionProps) {
+{/*
+    Utilizando a importação dos componetes props para os botões terem a mesma função de onclick 
+*/}
+{/*
+    dentro do componentes de button faz com que eu tenha todos os atrubutos deste elemento button
+*/}
+type ItemSuggestionProps = ComponentProps <'button'> & {
+    title: string
+    
+}
+{/*
+   Usando uma tecnica de espalhamento no props
+*/}
+export function ItemSuggestion({title,...props}: ItemSuggestionProps) {
     return(
-        <button>{title}</button>
+        <button {...props}>{title}</button>
     )
 }
