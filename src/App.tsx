9fb67053154f86b6522b61a2d63bcff900c7693c
Lucas Araujo <lucasara1707@gmail.com>
2024-10-19 16:28:46 -0300

    adicionando resposta com array de chat

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { ItemSuggestion } from "./components/ItemSuggestion"
 
 type ProgressType = 'pending' | 'started' | 'done'
 //pending deixara de ser pendente e começa a se inicializar e termina, assim: 1-pendente 2-começou 3- termino
+
+const chat = ['Tema']
 function App() {
   const [progess, setProgress] = useState<ProgressType>('pending')
   const [textarea, setTextArea] = useState<string>('')
@@ -50,52 +52,60 @@ function App() {
         {progess !== 'pending' && (
         
         <div className="box-chat">
-          <h1>Você está estudando sobre <span>HTML</span></h1>
+          {chat[0] && (
+            <h1>Você está estudando sobre <span>HTML</span></h1>
+          )}
 
-          <div className="question">
-            <h2><img src="./assets/question.svg" /> Pergunta</h2>
-            <p>
-              Claro! Aqui está a pergunta simulada:
-              "Como você descreveria o seu conhecimento
-              e experiência com HTML? Você poderia
-              fornecer um exemplo de um projeto em
-              que utilizou HTML e como isso impactou
-              positivamente o resultado final?"
-              Aguardo a sua resposta para poder
-              fornecer feedback!
-            </p>
-          </div>
+          {chat[1] && (
+               <div className="question">
+               <h2><img src="./assets/question.svg" /> Pergunta</h2>
+               <p>
+                 Claro! Aqui está a pergunta simulada:
+                 "Como você descreveria o seu conhecimento
+                 e experiência com HTML? Você poderia
+                 fornecer um exemplo de um projeto em
+                 que utilizou HTML e como isso impactou
+                 positivamente o resultado final?"
+                 Aguardo a sua resposta para poder
+                 fornecer feedback!
+               </p>
+             </div>
+          )}
 
-          <div className="answer">
+         {chat[2] && (
+           <div className="answer">
             <h2>Sua Resposta</h2>
             <p>
-              Tenho um conhecimento sólido em HTML.
-              Já construi vários sites
+             Tenho um conhecimento sólido em HTML.
+             Já construi vários sites
             </p>
           </div>
+         )}
 
-          <div className="feedback">
-            <h2>Feedback teach.me<span>.me</span></h2>
-            <p>
-              Ótimo! Parece que você tem uma boa experiência
-              com HTML e construiu vários sites. É importante
-              sempre destacar a prática e os projetos
-              realizados durante uma entrevista de emprego.
-              No entanto, vale ressaltar que seria
-              interessante fornecer um exemplo específico
-              de um projeto em que você utilizou HTML e
-              como suas habilidades impactaram positivamente
-              o resultado final. Isso pode ajudar a destacar
-              suas habilidades de forma mais concreta e
-              transmitir confiança ao entrevistador.
-              Espero que esse feedback seja útil e esteja
-              à disposição para mais perguntas ou informações
-              adicionais.
+          {chat[3] && (
+            <div className="feedback">
+                <h2>Feedback teach.me<span>.me</span></h2>
+                <p>
+                  Ótimo! Parece que você tem uma boa experiência
+                  com HTML e construiu vários sites. É importante
+                  sempre destacar a prática e os projetos
+                  realizados durante uma entrevista de emprego.
+                  No entanto, vale ressaltar que seria
+                  interessante fornecer um exemplo específico
+                  de um projeto em que você utilizou HTML e
+                  como suas habilidades impactaram positivamente
+                  o resultado final. Isso pode ajudar a destacar
+                  suas habilidades de forma mais concreta e
+                  transmitir confiança ao entrevistador.
+                  Espero que esse feedback seja útil e esteja
+                  à disposição para mais perguntas ou informações
+                  adicionais.
             </p>
             <div className="actions">
               <button>Estudar novo tópico sobre tecnologias e afins</button>
             </div>
           </div>
+          )}
         </div>
         )}
         
